21146adca19a3dedcbc83aa6563199bbff15fc46
Tanner Smith <smitht4@students.rowan.edu>
2017-10-16 00:56:18 -0400

    Added Commments

--- Addressable LED Code/uart.c
+++ Addressable LED Code/uart.c
@@ -16,16 +16,14 @@ int tempBuf = 0;
 
 void UART_Init(){
 
-    //UART Config
-    if (CALBC1_1MHZ==0xFF){                 // If calibration constant erased
-        while(1);                               // do not load, trap CPU!!
+    if (CALBC1_1MHZ==0xFF){                   // If calibration constant erased
+        while(1);                             // do not load, trap CPU!!
     }
-    WDTCTL = WDTPW | WDTHOLD;                 // stop watchdog timer
     DCOCTL = 0;                               // Select lowest DCOx and MODx settings
     BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
     DCOCTL = CALDCO_1MHZ;
-    P1SEL |= BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
-    P1SEL2 |= BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
+    P1SEL |= BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
+    P1SEL2 |= BIT1 + BIT2 ;                   // P1.1 = RXD, P1.2=TXD
     UCA0CTL1 |= UCSSEL_2;                     // SMCLK
     UCA0BR0 = 104;                            // 1MHz 9600
     UCA0BR1 = 0;                              // 1MHz 9600
@@ -38,32 +36,32 @@ void UART_Init(){
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-    while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+    while (!(IFG2&UCA0TXIFG));                // Wait till USCI_A0 TX buffer ready
 
     switch(bytecount){
-        case 0: numBytes = UCA0RXBUF;
+        case 0: numBytes = UCA0RXBUF;         // Store the number of bytes in packet
               break;
-        case 1: new_red = UCA0RXBUF;
+        case 1: new_red = UCA0RXBUF;          // Store the red value
               break;
-        case 2: new_green = UCA0RXBUF;
+        case 2: new_green = UCA0RXBUF;        // Store the green value
               break;
-        case 3: new_blue = UCA0RXBUF;
-              UCA0TXBUF = numBytes-3;
-              SetRed(new_red);
-              SetBlue(new_blue);
-              SetGreen(new_green);
+        case 3: new_blue = UCA0RXBUF;         // Store the blue value
+              UCA0TXBUF = numBytes-3;         // Send the first byte of new packet length
+              SetRed(new_red);                // Latch red value
+              SetGreen(new_green);            // Latch green value
+              SetBlue(new_blue);              // Latch blue value
               break;
         default:
           if(bytecount<numBytes)
           {
-              UCA0TXBUF = UCA0RXBUF;
+              UCA0TXBUF = UCA0RXBUF;          // Send out rest of packet as soon as its received
           }
           else{
-              bytecount = -1;
+              bytecount = -1;                 // End of packet, reset bytecount
           }
 
         }
-    bytecount++;
+    bytecount++;                              // Always increment bytecount
 }
 
 
